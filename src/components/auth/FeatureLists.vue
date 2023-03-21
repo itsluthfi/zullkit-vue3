831468696604b8ed3076fe535e925cd6f3587343
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-03-21 22:10:53 +0700

    refactor(register-page): convert register page to vue components

---
+++ src/components/auth/FeatureLists.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Customizable from '@/components/auth/features/Customizable.vue'
+import Fortune from '@/components/auth/features/Fortune.vue'
+import Documentation from '@/components/auth/features/Documentation.vue'
+</script>
+
+<template>
+    <Customizable />
+    <Fortune />
+    <Documentation />
+</template>
