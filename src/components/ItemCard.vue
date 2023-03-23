9d2048dc34e4210c15ae89357aeb4c312fdee990
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-03-23 15:22:12 +0700

    feat(details-page): add dynamic route using items id

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -2,6 +2,7 @@
 import { RouterLink } from 'vue-router'
 
 defineProps({
+    id: Number,
     title: String,
     image: String,
     category: String,
@@ -12,7 +13,7 @@ defineProps({
 <template>
     <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
         <div class="overflow-hidden border border-gray-200 rounded-xl">
-            <RouterLink to="/">
+            <RouterLink :to="'/details/' + id">
                 <div class="m-4 overflow-hidden rounded-xl">
                     <img :alt="title" class="block w-full h-auto" :src="'src/assets/img/' + image" />
                 </div>
--- src/components/homepage/Items.vue
+++ src/components/homepage/Items.vue
@@ -13,7 +13,7 @@ const items = ref([
     <div class="container px-4 mx-auto my-16 md:px-12">
         <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
         <div class="flex flex-wrap -mx-1 lg:-mx-4">
-            <ItemCard v-for="item in items" :key="item.id" :title="item.title" :category="item.category"
+            <ItemCard v-for="item in items" :key="item.id" :id="item.id" :title="item.title" :category="item.category"
                 :image="item.image" />
         </div>
     </div>
