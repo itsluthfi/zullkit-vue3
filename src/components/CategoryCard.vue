0bb9360c384e6bd895823c1d82cf6355c91ff39e
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-03-24 23:36:24 +0700

    feat(category-items): add dynamic parameters and integrate items with backend api

--- src/components/CategoryCard.vue
+++ src/components/CategoryCard.vue
@@ -2,32 +2,34 @@
 import { RouterLink } from 'vue-router'
 
 defineProps({
-    title: String,
-    image: String,
-    count: Number,
+  id: Number,
+  title: String,
+  image: String,
+  count: Number,
 }) // defineProps
 
 </script>
 
 <template>
-    <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
-        <div class="overflow-hidden border border-gray-200 rounded-xl">
-            <RouterLink to="/">
-                <div class="m-4 overflow-hidden rounded-xl">
-                    <img :alt="title" class="block w-full h-auto" :src="image" />
-                </div>
-            </RouterLink>
-
-            <header class="px-4 mb-4 leading-tight">
-                <h1 class="text-lg">
-                    <RouterLink class="font-semibold text-black no-underline hover:underline" to="/">
-                        {{ title }}
-                    </RouterLink>
-                </h1>
-                <span class="block text-sm font-light text-gray-500 no-underline">
-                    {{ count }} items
-                </span>
-            </header>
+  <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
+    <div class="overflow-hidden border border-gray-200 rounded-xl">
+      <RouterLink :to="{ name: 'category-items', params: { id: id } }">
+        <div class="m-4 overflow-hidden rounded-xl">
+          <img :alt="title" class="block w-full h-auto" :src="image" />
         </div>
+      </RouterLink>
+
+      <header class="px-4 mb-4 leading-tight">
+        <h1 class="text-lg">
+          <RouterLink class="font-semibold text-black no-underline hover:underline"
+            :to="{ name: 'category-items', params: { id: id } }">
+            {{ title }}
+          </RouterLink>
+        </h1>
+        <span class="block text-sm font-light text-gray-500 no-underline">
+          {{ count }} items
+        </span>
+      </header>
     </div>
+  </div>
 </template>
