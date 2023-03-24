ef1ec76c9d7b5ca491711cbc254b3560b08e7294
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-03-24 17:02:34 +0700

    feat(homepage): integrate categories section and items section with backend api

--- src/components/homepage/Categories.vue
+++ src/components/homepage/Categories.vue
@@ -1,21 +1,31 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
 import CategoryCard from '@/components/CategoryCard.vue'
 
-const categories = ref([
-    { id: 1, title: 'Mobile UI Kit', 'count': 731, 'image': 'categories-1.jpg' },
-    { id: 2, title: 'Fonts', 'count': 657, 'image': 'categories-2.jpg' },
-    { id: 3, title: 'Icon Set', 'count': 85339, 'image': 'categories-3.jpg' },
-    { id: 4, title: 'Website UI Kit', 'count': 4500, 'image': 'categories-4.jpg' },
-])
+const categories = ref([])
+
+async function getCategoriesData() {
+    try {
+        const response = await axios.get('https://zullkit-backend.buildwithangga.id/api/categories?show_product=1&limit=4')
+        categories.value = response.data.data.data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+onMounted(() => {
+    getCategoriesData()
+})
 </script>
 
 <template>
     <div class="container px-4 mx-auto my-16 md:px-12">
         <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">Top Categories</h2>
         <div class="flex flex-wrap -mx-1 lg:-mx-4">
-            <CategoryCard v-for="category in categories" :key="category.id" :title="category.title" :count="category.count"
-                :image="category.image" />
+            <CategoryCard v-for="category in categories" :key="category.id" :title="category.name"
+                :count="category.products.length" :image="category.thumbnails" />
         </div>
     </div>
 </template>
--- src/components/homepage/Items.vue
+++ src/components/homepage/Items.vue
@@ -1,20 +1,31 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
 import ItemCard from '@/components/ItemCard.vue'
 
-const items = ref([
-    { id: 1, title: 'RoboCrypto UI Kit', category: 'Mobile UI Kit', image: 'items-1.jpg' },
-    { id: 2, title: 'Online Doctor Consultation', category: 'Website UI Kit', image: 'items-2.jpg' },
-    { id: 3, title: 'Banking Crypto', category: 'Mobile UI Kit', image: 'items-3.jpg' },
-])
+const items = ref([])
+
+async function getItemsData() {
+    try {
+        const response = await axios.get('https://zullkit-backend.buildwithangga.id/api/products?limit=3')
+        items.value = response.data.data.data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+onMounted(() => {
+    getItemsData()
+})
 </script>
 
 <template>
     <div class="container px-4 mx-auto my-16 md:px-12">
         <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
         <div class="flex flex-wrap -mx-1 lg:-mx-4">
-            <ItemCard v-for="item in items" :key="item.id" :id="item.id" :title="item.title" :category="item.category"
-                :image="item.image" />
+            <ItemCard v-for="item in items" :key="item.id" :id="item.id" :title="item.name" :category="item.subtitle"
+                :image="item.thumbnails" />
         </div>
     </div>
 </template>
