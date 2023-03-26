e1a1af77e34a6f1347483bbee95f7206e139f9c9
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-03-27 06:37:31 +0700

    feat: add navigation link on each page
    style: change indent space from 4 to 2 and formatting files

--- src/components/homepage/Categories.vue
+++ src/components/homepage/Categories.vue
@@ -21,7 +21,7 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="container px-4 mx-auto my-16 md:px-12">
+  <div id="categories" class="container px-4 mx-auto my-16 md:px-12">
     <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">Top Categories</h2>
     <div class="flex flex-wrap -mx-1 lg:-mx-4">
       <CategoryCard v-for="category in categories" :key="category.id" :id="category.id" :title="category.name"
