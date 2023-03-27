63b5dda7a07e311cc6d6942a66395671eca3cd93
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-03-27 07:22:04 +0700

    style: change indent space from 4 to 2 and formatting files

--- src/components/homepage/Items.vue
+++ src/components/homepage/Items.vue
@@ -7,25 +7,25 @@ import ItemCard from '@/components/ItemCard.vue'
 const items = ref([])
 
 async function getItemsData() {
-    try {
-        const response = await axios.get('https://zullkit-backend.buildwithangga.id/api/products?limit=3')
-        items.value = response.data.data.data
-    } catch (error) {
-        console.error(error)
-    }
+  try {
+    const response = await axios.get('https://zullkit-backend.buildwithangga.id/api/products?limit=3')
+    items.value = response.data.data.data
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 onMounted(() => {
-    getItemsData()
+  getItemsData()
 })
 </script>
 
 <template>
-    <div class="container px-4 mx-auto my-16 md:px-12">
-        <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
-        <div class="flex flex-wrap -mx-1 lg:-mx-4">
-            <ItemCard v-for="item in items" :key="item.id" :id="item.id" :title="item.name" :category="item.subtitle"
-                :image="item.thumbnails" />
-        </div>
+  <div class="container px-4 mx-auto my-16 md:px-12">
+    <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
+    <div class="flex flex-wrap -mx-1 lg:-mx-4">
+      <ItemCard v-for="item in items" :key="item.id" :id="item.id" :title="item.name" :category="item.subtitle"
+        :image="item.thumbnails" />
     </div>
+  </div>
 </template>
--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -6,12 +6,12 @@ import UserInfo from './UserInfo.vue'
 </script>
 
 <template>
-    <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-        <div class="container flex flex-wrap items-center justify-between mx-auto my-2">
-            <Logo />
-            <NavigationLinks />
-            <AuthButton />
-            <!-- <UserInfo /> -->
-        </div>
-    </nav>
+  <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+    <div class="container flex flex-wrap items-center justify-between mx-auto my-2">
+      <Logo />
+      <NavigationLinks />
+      <AuthButton />
+      <!-- <UserInfo /> -->
+    </div>
+  </nav>
 </template>
