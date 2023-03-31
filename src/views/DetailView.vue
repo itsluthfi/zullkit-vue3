959699fb02c4453bb673d7d810bae93b1dc48f8e
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-03-31 20:51:13 +0700

    fix(details-page): resolve blank page when accessing page without login

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -79,10 +79,12 @@ onMounted(() => {
                   </li>
                 </ul>
               </div>
-              <a v-if="user.data.subscription.length > 0" :href="item.file" target="_blank"
-                class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-full hover:bg-indigo-700 md:py-2 md:text-md md:px-10 hover:shadow">
-                Download Now
-              </a>
+              <div v-if="user.data">
+                <a v-if="user.data.subscription.length > 0" :href="item.file"
+                  class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-full hover:bg-indigo-700 md:py-2 md:text-md md:px-10 hover:shadow ">
+                  Download Now
+                </a>
+              </div>
               <RouterLink v-else to="/pricing"
                 class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-full hover:bg-indigo-700 md:py-2 md:text-md md:px-10 hover:shadow">
                 Subscribe
